85f70ca144be7a252459880f7a5e2f7c3d74f3d3
Peter <peter.zdankin@uni-due.de>
2021-10-27 17:50:58 +0200

    Prime

---
+++ code/U1/prime.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int isPrime(int zahl){
+    for(int i = 2; i < zahl - 1; i = i+1){
+        if (zahl % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int zahl = 13;
+    if(isPrime(zahl)){
+        printf("Is Prime");
+    }else{
+        printf("Is not");
+    }
+    return 0;
+}
